eed3afbded32a994a3244b847c5f80a0a5f7ca0e
bentleyminwa <bentleyminwa@gmail.com>
2025-04-11 11:28:57 +0300

    Implement Cart model,  schema and types

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,17 @@
-import { insertProductSchema } from "@/lib/validator";
+import {
+  cartItemSchema,
+  insertCartSchema,
+  insertProductSchema,
+} from "@/lib/validator";
 import { z } from "zod";
 
+// Product
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+// Cart
+export type CartItem = z.infer<typeof cartItemSchema>;
+export type Cart = z.infer<typeof insertCartSchema>;
